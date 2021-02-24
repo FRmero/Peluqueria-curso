3bfa6d8c90ac609e2fda0934bce05434c73e471c
unknown <soy.facuromero@gmail.com>
2021-02-24 08:27:47 -0300

    Added tabs, form and services

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+let pagina = 1;
 
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
@@ -5,8 +6,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function iniciarApp() {
     mostrarServicios();
+
+    // Resalta div actual según el tab que se presiona
+    mostrarSeccion();
+
+    // Oculta o muestra una sección según al tab al que se presiona
+    cambiarSeccion();
+
+    // Paginacion siguiente y anterior
+    paginaSiguiente();
+
+    paginaAnterior();
+
+    // Comprueba página actual para ocultar la paginación
+    botonesPaginador();
+
 }
 
+function mostrarSeccion() {
+
+    // Eliminar mostrar-seccion de la seccion anterior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if(seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar-seccion');
+    }
+    
+
+    
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    // Eliminar la clase de actual en el tab anterior
+    const tabAnterior = document.querySelector('.tabs .actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+    
+
+    // Resalta tab actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`); // Va entre corchetes porque es un atributo
+    tab.classList.add('actual');
+}
+
+function cambiarSeccion() {
+    const enlaces = document.querySelectorAll('.tabs button');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+            pagina = parseInt(e.target.dataset.paso);
+
+            // Llamar la función de mostrar sección
+            mostrarSeccion();
+            botonesPaginador();
+        })
+    })
+}
+
+/* Servicios */
 async function mostrarServicios() {
     try {
         const resultado = await fetch('./servicios.json');
@@ -34,7 +91,7 @@ async function mostrarServicios() {
             servicioDiv.classList.add('servicio');
 
             // Seleccionar servicios
-            servicioDiv.onclick = seleccionarServicio;
+            servicioDiv.onclick = seleccionarServicio; //onclick es un eventhandler. Se utiliza mayormente en código creado en JS, mientras que addeventlistener en código ya existente.
 
             // Agregar precio y nombre al contenedor
             servicioDiv.appendChild(nombreServicio);
@@ -55,12 +112,48 @@ function seleccionarServicio(e) {
     if(e.target.tagName === 'P') {
         elemento = e.target.parentElement;
     } else {
-        e.target;
+       elemento = e.target;
     }
 
-    if(elemento.classList.contains('seleccionado')){
+   if(elemento.classList.contains('seleccionado')){
         elemento.classList.remove('seleccionado');
     } else {
         elemento.classList.add('seleccionado');
+    } 
+}
+
+/* Paginacion */
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector("#siguiente");
+    paginaSiguiente.addEventListener('click', () => {
+        pagina++;
+
+        botonesPaginador();
+    });
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector("#anterior");
+    paginaAnterior.addEventListener('click', () => {
+        pagina--;
+
+        botonesPaginador();
+    });
+}
+
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector("#siguiente");
+    const paginaAnterior = document.querySelector("#anterior");
+
+    if(pagina === 1 ) {
+        paginaAnterior.classList.add('ocultar');
+    } else if (pagina === 3) {
+        paginaSiguiente.classList.add('ocultar');
+        paginaAnterior.classList.remove('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
     }
+
+    mostrarSeccion();
 }
